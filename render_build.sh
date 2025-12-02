8db567587538324c245af2a52455db0cafd959be
Ehiber Graterol <56272634+ehiber@users.noreply.github.com>
2025-12-02 13:50:47 -0400

    Enhance render_build.sh with pipenv setup
    Added pipenv installation commands to the build script.

--- render_build.sh
+++ render_build.sh
@@ -2,9 +2,17 @@
 # exit on error
 set -o errexit
 
+#!/usr/bin/env bash
+
+# exit on error
+set -o errexit
+
 npm install
 npm run build
 
+pip install pipenv 
+
+pipenv install
 pipenv install
 
 pipenv run upgrade
